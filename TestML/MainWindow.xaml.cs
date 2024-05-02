a13d62262f0ed44f6920597f1cadb02cb8e6fa80
AlexD972 <alexandreduclos83@gmail.com>
2024-05-02 21:10:00 +0200

    Ajoutez des fichiers projet.

---
+++ TestML/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestML
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Btn_Charger_Img_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == true)
+			{
+				Uri fileUri = new Uri(openFileDialog.FileName);
+				img.Source = new BitmapImage(fileUri);
+			}
+		}
+		private void Btn_Classifier_Click(object sender, RoutedEventArgs e)
+		{
+			var input = new ModelInput();
+			input.ImageSource = img.Source;
+
+			ModelOutput result = ConsumeModel.Predict(input);
+
+			MessageBox.Show("La classe du navire est : "+ result.Prediction);
+		}
+		
+	}
+}
